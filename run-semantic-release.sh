a44271a8db76c7cf1558d7125f63dba995b8ea52
donnettech <obiora_chuck@hotmail.com>
2023-08-05 23:38:50 -0400

    fix: sementic1 versioning

---
+++ run-semantic-release.sh
@@ -0,0 +1,9 @@
+# Navigate to the repository's root directory
+cd /path/to/your/repository
+
+# Create the shell script file (run-semantic-release.sh)
+echo "npx semantic-release" > run-semantic-release.sh
+
+# Make the script executable
+chmod +x run-semantic-release.sh
+
