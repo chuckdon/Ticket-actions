1a3d59360e9419efaca45a075fa2669df00458ee
donnettech <obiora_chuck@hotmail.com>
2023-08-08 19:49:14 -0400

    feat: 2minor version

--- main.tf
+++ main.tf
@@ -5,10 +5,10 @@ provider "aws" {
 
 resource "aws_vpc" "actions" {
   cidr_block = "10.0.0.0/16"
-  
+
   tags = {
-    Name  = "donnettech"
-    team  = "devops"
-  
+    Name = "donnettech"
+    team = "devops"
+
   }
 }
