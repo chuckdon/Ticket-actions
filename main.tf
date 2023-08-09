517507abce03abf7e3324c0e1d79bc12b0067f29
donnettech <obiora_chuck@hotmail.com>
2023-08-08 21:32:32 -0400

    fix: added tag

--- main.tf
+++ main.tf
@@ -7,8 +7,9 @@ resource "aws_vpc" "actions" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "donnettech"
-    team = "devops"
+    Name        = "donnettech"
+    team        = "devops"
+    Environment = "prod"
 
   }
 }
